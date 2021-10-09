01949da8a0db0d66a1ba80c5a6fa03b3bb64ac14
michael-pulliam <85899154+michael-pulliam@users.noreply.github.com>
2021-10-09 15:16:22 -0500

    Delete API2.js

--- week-2/API2.js
+++
@@ -1,9 +0,0 @@
-[
-    {
-        "breeds": [],
-        "id": "aJn2siGcP",
-        "url": "https://cdn2.thecatapi.com/images/aJn2siGcP.jpg",
-        "width": 3264,
-        "height": 2448
-    }
-]
